af11d20c52402050d3e372ab6897287c2f049d43
Федор Шарапов <sharapovfedor@yandex.ru>
2023-03-10 14:50:24 +0100

    реализацию IRepository в виде EfRepository

---
+++ src/Otus.Teaching.PromoCodeFactory.DataAccess/Repositories/EfRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+using Otus.Teaching.PromoCodeFactory.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otus.Teaching.PromoCodeFactory.DataAccess.Repositories
+{
+    public class EfRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly DbContext Context;
+        protected DbSet<T> EntitySet;
+
+        public EfRepository(DbContext context)
+        {
+            Context = context;
+            EntitySet = Context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await EntitySet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await EntitySet.FindAsync(id);
+        }
+    }
+}
